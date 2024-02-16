dba75450f02fe5aa6f1dcdc95219f42b3d02ebf2
Robin2bin <72858291+Arko2bin@users.noreply.github.com>
2024-02-16 22:50:05 +0530

    PDF VIWER UPDATE

--- Onedrive.py
+++ Onedrive.py
@@ -130,7 +130,7 @@ def admin():
                     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
                 # Embedding PDF in HTML
-                pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+                pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
 
                 # Displaying File
                 st.markdown(pdf_display, unsafe_allow_html=True)
